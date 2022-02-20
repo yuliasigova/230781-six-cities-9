6060572ec7d9fe0584aa2826a4b55cece2a0a256
yuliasigova <yulya12yulya@gmail.com>
2022-02-20 18:39:47 +0400

    Добавила маршрутизацию

---
+++ project/src/const.tsx
@@ -0,0 +1,14 @@
+export enum AppRoute {
+  Login = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id',
+  Main = '/',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+
